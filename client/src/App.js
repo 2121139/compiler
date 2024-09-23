d2fdc94dc11adf7bd6ee272de575bfb3f0e5ddbf
Amanpreet Kaur <109809313+2121139@users.noreply.github.com>
2024-09-23 14:20:00 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -13,11 +13,9 @@ import CompilerCodePage from './CompilerCodePage';
 import ProblemPage from './ProblemPage'
 import PythonTutorials from './PythonTutorials';
 import CppTutorials from './CppTutorials';
-import axios from 'axios';
 
 function App() {
    const [codeHistory, setCodeHistory] = useState([]); 
-   axios.defaults.withCredentials = true;
    const handleSubmit = (e) => {
       e.preventDefault();
       axios.post('https://compiler-apis.vercel.app/')
